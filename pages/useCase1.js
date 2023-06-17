1c43bdf69ce1b74db212308736f8151e65aadd46
toumelin <atoumelin@yahoo.fr>
2023-06-17 14:31:51 -0400

    link usecase

---
+++ pages/useCase1.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import CenteredCTAText from "../components/ui/CenteredCTAText";
+import CTA from "../components/ui/CTA";
+import FAQs from "../components/ui/FAQs";
+import Features from "../components/ui/Features";
+import FooterCTA from "../components/ui/FooterCTA";
+import Hero from "../components/ui/Hero";
+import LogoGrid from "../components/ui/LogoGrid";
+import Stats from "../components/ui/Stats";
+import Navbar from "../components/ui/Navbar";
+import Footer from "../components/ui/Footer";
+import DecisionMaking from "../components/ui/DecisionMaking";
+import DataIngestion from "../components/ui/DataIngestion";
+import InsightGen from "../components/ui/InsightGen";
+import DataGov from "../components/ui/DataGovernance"
+import Graphmap from "../components/ui/Graphmap";
+import Carroussel from "../components/ui/Carroussel";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name='robots' content='index' />
+      </Head>
+      <Navbar />
+      <Hero />
+      {/* <CenteredCTAText />
+      <CTA /> */}
+      {/* <Features /> */}
+      {/* <Stats /> */}
+      {/* <FAQs /> */}
+      {/* <FooterCTA /> */}
+      <Footer />
+    </>
+  );
+}
