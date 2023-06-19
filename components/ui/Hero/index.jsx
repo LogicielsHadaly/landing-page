d48030cb8c969b3aa42a977a0a2083293bce8490
toumelin <atoumelin@yahoo.fr>
2023-06-18 23:48:24 -0400

    place

--- components/ui/Hero/index.jsx
+++ components/ui/Hero/index.jsx
@@ -99,7 +99,7 @@ export default function Example() {
 
   return (
     <div className="relative overflow-hidden bg-white">
-      <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+      <div className="pb-80 pt-8 sm:pb-40 sm:pt-24 lg:pb-40 lg:pt-35">
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <div className="sm:max-w-lg">
             <h1 className="text-5xl text-gray-800 mx-auto sm:text-6xl">
