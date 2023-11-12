e37af5565e754431f1ea0ede365e84e6ee6e2b17
LogicielsHadaly <atoumelin@yahoo.fr>
2023-11-12 13:47:30 -0500

    add clarity

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
   // const router = useRouter();
+
   return (
     <>
       <Head>
@@ -27,6 +28,20 @@ export default function App({ Component, pageProps }) {
           as="font"
           crossOrigin=""
         />
+
+        {/* Your custom script */}
+        <script 
+          dangerouslySetInnerHTML={{ 
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "jph051lmfg");
+            ` 
+          }} 
+        />
+
         <style>{`
           @font-face {
             font-family: 'CustomFont';
@@ -47,8 +62,8 @@ export default function App({ Component, pageProps }) {
           }
         `}</style>
       </Head>
+
       <Component {...pageProps} />
     </>
   );
 }
-
